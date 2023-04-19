5ebf8fa644ffe38ae1222ae733f4f3ac7ff27588
s26971 <s26971@pjwstk.edu.pl>
2023-04-19 16:29:04 +0200

    Poprawa literówek

--- migration/V2_lab05.sql
+++ migration/V2_lab05.sql
@@ -2,32 +2,32 @@ DELETE FROM obszar; --Usunięcie wszystkich danych z bazy
 
 DROP TABLE  pasazer;
 CREATE TABLE pasazer (
-    id serial PRIMARY KEY,
-  uzytkownik_id int UNIQUE ,
-  preferowanytyppłatności int); --Usunięcie i dodanie tabeli na nowo
+                         id serial PRIMARY KEY,
+                         uzytkownik_id int UNIQUE ,
+                         preferowanytyppłatności int); --Usunięcie i dodanie tabeli na nowo
 
-ALTER TABLE pasazer
-    ADD CONSTRAINT fk
-        foregin KEY (uzytkownik_id) REFERENCES uzytkownik (id)
-        ON UPDATE CASCADE; --Dodanie klucza obcego
+alter table pasazer
+    add constraint fk
+        foreign key (uzytkownik_id) references uzytkownik
+            on update cascade; --Dodanie klucza obcego
 
-INSERT INTO 
-  pasażer (uzytkownik_id, preferowanytyppłatności)
+INSERT INTO
+    pasazer (uzytkownik_id, preferowanytyppłatności)
 VALUES
-  ('1', '1'),
-  ('2', '2'),
-  ('3','1');
+    ('1', '1'),
+    ('2', '2'),
+    ('3','1');
 
 UPDATE pasazer SET preferowanytyppłatności = 3 WHERE id = 1;
 UPDATE pasazer SET preferowanytyppłatności = 4 WHERE id = 2;
 
-ALTER TABLE pasazer
-    ADD CONSTRAINT fk_2
-        FOREGIN KEY (preferowanytyppłatności) REFERENCES typplatności (id)
-        ON UPDATE CASCADE;
+alter table pasazer
+    add constraint fk_2
+        foreign key (preferowanytyppłatności) references typplatnosci (id)
+            on update cascade;
 
-ALTER TABLE kierowca
-    ADD CONSTRAINT "kier-user-fk"
-        FOREGIN KEY (uzytkownik_id) REFERENCES uzytkownik;
+alter table kierowca
+    add constraint "kier-user-fk"
+        foreign key (uzytkownik_id) references uzytkownik;
 
-        UPDATE uzytkownik SET id = 5 WHERE id = 2;
+UPDATE uzytkownik SET id = 5 WHERE id = 2;
